96cbd3544dd6da2bfa5dd74674ac9e109a39c274
Riley Wells <rileymorganwells@gmail.com>
2018-09-02 16:20:36 -0600

    Came up with all questions, answers, and corresponding characters, as well as images for each

--- main.js
+++ main.js
@@ -1,56 +1,121 @@
 $(document).ready(function(){
-    $('#main-container').fadeIn(3000);
+    $('#main-container').fadeIn(2000);
 });
 
 function beginQuiz() {
     $('#main-container').fadeOut(2000);
     $('#quiz-container').delay(2000).fadeIn(2000);
-
-    // var Questions = [
-    //     ['Which is your preferred means of transportation?','Car','Train','Horse','Foot'],
-    //     ['Question 2','1','2','3','4']
-    // ];
     
     const myQuestions = [
       {
-        question: "Who is the strongest?",
-        answers: {
-          a: "Superman",
-          b: "The Terminator",
-          c: "Waluigi, obviously",
-          d: "Riley"
-        },
-        correctAnswer: "c"
-      },
-      {
-        question: "What is the best site ever created?",
-        answers: {
-          a: "SitePoint",
-          b: "Simple Steps Code",
-          c: "Trick question; they're both the best",
-          d: "rileymorganwells.github.io"
-        },
-        correctAnswer: "c"
-      },
-      {
-        question: "Where is Waldo really?",
-        answers: {
-          a: "Antarctica",
-          b: "Exploring the Pacific Ocean",
-          c: "Sitting in a tree",
-          d: "Minding his own business, so stop asking"
-        },
-        correctAnswer: "d"
-      },
-      {
-        question: "Which is your preferred means of transportation?",
+        question: "Which is your preferred method of transportation?",
         answers: {
             a: "Car",
             b: "Train",
             c: "Horse",
             d: "Foot"
         },
-        correctAnswer: "b"
+        correctAnswer: "b",
+        image: "images/q1.png"
+        },
+      {
+        question: "You notice a pretty woman drop her bag. What do you do?",
+        answers: {
+          a: "Pick it up. After all, you are quite chivalrous",
+          b: "Ignore her - you're not interested",
+          c: "Pick it up and keep it for yourself",
+          d: "Bring it to her - tell her she owes you"
+        },
+        correctAnswer: "c",
+        image: "images/q2.png"
+      },
+      {
+        question: "What is your view of humanity?",
+        answers: {
+          a: "Humanity's reign is coming to an end",
+          b: "Humans, at their core, are evil",
+          c: "Humans, at their core, are good",
+          d: "Humans are a mix of good and bad - our choices make us who we are"
+        },
+        correctAnswer: "c",
+        image: "images/q3.png"
+      },
+      {
+        question: "Pick a Shakespeare quote",
+        answers: {
+          a: "\"These violent delights have violent ends\"",
+          b: "\"When we are born, we cry that we are come to this great stage of fools\"",
+          c: "\"Hell is empty and all the devils are here\"",
+          d: "\"For in that sleep of death, what dreams may come?\""
+        },
+        correctAnswer: "d",
+        image: "images/q4.png"
+      },
+      {
+        question: "How does this image make you feel?",
+        answers: {
+          a: "Excited",
+          b: "Nervous",
+          c: "At home",
+          d: "Angry"
+        },
+        correctAnswer: "d",
+        image: "images/q5.png"
+      },
+      {
+        question: "Your ideal Friday night consists of:",
+        answers: {
+          a: "Hanging out with your dog - people are the worst",
+          b: "Spending time with your significant other",
+          c: "Curling up in bed with a good book",
+          d: "Having a blast - regardless of the consequences"
+        },
+        correctAnswer: "d",
+        image: "images/q6.png"
+      },
+      {
+        question: "Which technology do you find most intriguing?",
+        answers: {
+          a: "Machine learning/AI",
+          b: "Self-driving cars",
+          c: "Firearms",
+          d: "Robotics"
+        },
+        correctAnswer: "d",
+        image: "images/q7.png"
+      },
+      {
+        question: "If you lived in the Old West, you would be:",
+        answers: {
+          a: "The indian",
+          b: "The cowgirl",
+          c: "The outlaw",
+          d: "The sheriff"
+        },
+        correctAnswer: "d",
+        image: "images/q8.png"
+      },
+      {
+        question: "You find yourself in a maze. What's at the end?",
+        answers: {
+          a: "Life's great meaning",
+          b: "Nothing",
+          c: "I'd never make it to the end...",
+          d: "Another maze"
+        },
+        correctAnswer: "d",
+        image: "images/q9.png"
+      },
+      {
+        question: "What is your number one priority in life?",
+        answers: {
+            a: "Happiness",
+            b: "Family",
+            c: "Power",
+            d: "Belief"
+        },
+        correctAnswer: "b",
+        image: "images/q10.png"
       }
     ];
   
@@ -67,9 +132,8 @@ function beginQuiz() {
         for (letter in currentQuestion.answers) {
           // ...add an HTML radio button
           answers.push(
-            `<label class="btn btn-primary btn-lg">
+            `<label class="btn btn-lg bwbtn">
                <input type="radio" name="question${questionNumber}" value="${letter}">
-
                 ${currentQuestion.answers[letter]}
              </label><br>`
           );
@@ -78,7 +142,8 @@ function beginQuiz() {
         // add this question and its answers to the output
         output.push(
           `<div class="slide">
-             <div class="question"> ${currentQuestion.question} </div>
+             <img src="${currentQuestion.image}" class="img-slide"/>
+             <div class="question"><h2>${currentQuestion.question}</h2></div>
              <div class="answers"> ${answers.join("")} </div>
            </div>`
         );
@@ -114,9 +179,6 @@ function beginQuiz() {
       // show number of correct answers out of total
       resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
       resultsContainer.style.display = "inline-block";
-      //quizContainer.innerHTML = resultsContainer.innerHTML;
-        //   previousButton.style.display = "none";
-        //   submitButton.style.display = "none";
       restartButton.style.display = "inline-block";
       document.getElementById("current-question").style.display = "none";
     }
@@ -133,24 +195,13 @@ function beginQuiz() {
 
       var curr = "Question " + (currentSlide + 1) + " of " + myQuestions.length;
       document.getElementById("current-question").innerHTML = curr;
-
-      if (currentSlide === 0) {
-        // previousButton.style.display = "none";
-      } else {
-        // previousButton.style.display = "inline-block";
-      }
       
       if (currentSlide === slides.length - 1) {
-        // nextButton.style.display = "none";
-        // submitButton.style.display = "inline-block";
         document.querySelectorAll(".radio-btns").forEach(function(elem) {
             elem.removeEventListener("click",showNextSlide);
             elem.addEventListener("click", showResults);
           });
-      } else {
-        // nextButton.style.display = "inline-block";
-        // submitButton.style.display = "none";
-      }
+      } 
     }
   
     function showNextSlide() {
@@ -170,8 +221,8 @@ function beginQuiz() {
         document.querySelectorAll(".radio-btns").forEach(function(elem) {
             elem.addEventListener("click", showNextSlide);
         });
+
         restartButton.style.display = "none";
-        // nextButton.style.display = "inline-block";
         currentSlide = 0;
         var curr = "Question " + (currentSlide + 1) + " of " + myQuestions.length;
         document.getElementById("current-question").innerHTML = curr;
@@ -180,23 +231,15 @@ function beginQuiz() {
 
     const quizContainer = document.getElementById("quiz");
     const resultsContainer = document.getElementById("results");
-    // const submitButton = document.getElementById("submit");
   
     // display quiz
     buildQuiz();
   
-    // const previousButton = document.getElementById("previous");
-    // const nextButton = document.getElementById("next");
     const restartButton = document.getElementById("restart");
     const slides = document.querySelectorAll(".slide");
     let currentSlide = 0;
   
     showSlide(0);
   
-    // on submit, show results
-    // submitButton.addEventListener("click", showResults);
-    // previousButton.addEventListener("click", showPreviousSlide);
-    // nextButton.addEventListener("click", showNextSlide);
     restartButton.addEventListener("click", restartQuiz);
-
   }
